5e93dd7f0ccff792c810cb3b1c26e8932008e0b6
Christian S Sousa <78213318+ChstianSantos@users.noreply.github.com>
2025-02-24 21:36:38 -0300

    Update user_model.py

--- PJ-frame/app/models/user_model.py
+++ PJ-frame/app/models/user_model.py
@@ -1,8 +1,8 @@
 class User:
     # Simulação de um banco de dados
     users = [
-        {"id": 1, "name": "Chris", "email": "chris@example.com"},
-        {"id": 2, "name": "Ella", "email": "Ella@example.com"}
+        {"id": 1, "name": "Alice", "email": "alice@example.com"},
+        {"id": 2, "name": "Bob", "email": "bob@example.com"}
     ]
 
     @classmethod
@@ -14,4 +14,32 @@ class User:
         for user in cls.users:
             if user["id"] == user_id:
                 return user
+        return None
+    
+    @classmethod
+    def store(cls, data):
+
+        new_user = {
+            "id": len(cls.users) + 1,
+            "name": data["name"],
+            "email": data["email"]
+        }
+        cls.users.append(new_user)        
+        return cls.users
+    
+    @classmethod
+    def delete_user(cls, user_id):
+        for user in cls.users:
+            if user["id"] == user_id:
+                cls.users.remove(user) 
+                return True
+        return False 
+    
+    @classmethod
+    def update_user(cls, user_id, data):
+        for user in cls.users:
+            if user["id"] == user_id:
+                user["name"] = data.get("name", user["name"])
+                user["email"] = data.get("email", user["email"])
+                return user  
         return None
