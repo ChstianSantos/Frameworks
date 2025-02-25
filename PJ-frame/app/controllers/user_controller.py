0b3e49e57a41c3afaa3b5fd251007943f8ef2210
Christian S Sousa <78213318+ChstianSantos@users.noreply.github.com>
2025-02-24 21:37:23 -0300

    Update user_controller.py

--- PJ-frame/app/controllers/user_controller.py
+++ PJ-frame/app/controllers/user_controller.py
@@ -16,4 +16,57 @@ def get_user_by_id(user_id):
             mensagem = "Listagem de user",
             usuarios = User.get_user_by_id(user_id)
         )
+    )
+
+def store():
+    data = request.get_json()
+
+    name = data.get("name", "")
+    email = data.get("email", "")
+
+    if not name or not email:
+        return make_response(
+        jsonify(
+            mensagem = "error",
+            usuarios = "Os campos 'name' e 'email' são obrigatórios e não podem estar vazios."
+        ), 400
+    )         
+
+    return make_response(
+        jsonify(
+            mensagem = "user salvo com sucesso",
+            usuarios = User.store(data)
+        )
     ) 
+
+def delete_user_by_id(user_id):
+    success = User.delete_user(user_id)
+    if success:
+        return make_response(
+            jsonify(
+                mensagem="Usuário excluído com sucesso"
+            )
+        )
+    return make_response(
+        jsonify(
+            mensagem="Usuário não encontrado"
+        ), 404
+    )
+
+def update_user_by_id(user_id):
+    data = request.get_json()
+    updated_user = User.update_user(user_id, data)
+
+    if updated_user:
+        return make_response(
+            jsonify(
+                mensagem="Usuário atualizado com sucesso",
+                usuario=updated_user
+            )
+        )
+    
+    return make_response(
+        jsonify(
+            mensagem="Usuário não encontrado"
+        ), 404
+    )
